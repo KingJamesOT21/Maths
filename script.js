59e1b09a82232a9f8b1605f69fa4c64a2b2ec9f9
KingJamesOT21 <KingJamesOT21@gmail.com>
2025-03-17 13:46:59 +0200

    mobile responsive

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-console.log("Script starting...");
 
 const slider = document.querySelector(".slider");
 const textBoxes = document.querySelector(".text-boxes");
 const boxes = document.querySelectorAll(".text-boxes .box");
 const dots = document.querySelectorAll(".dot");
 
-console.log("Elements found:", {
-    slider: !!slider,
-    textBoxes: !!textBoxes,
-    boxes: boxes.length,
-    dots: dots.length
-});
 
 let currentIndex = 0;
 const totalBoxes = boxes.length;
@@ -18,13 +11,11 @@ let autoSlide;
 
 function updateSliderPosition() {
     const offset = -currentIndex * (300 / totalBoxes);
-    console.log("Sliding to offset:", offset);
     textBoxes.style.transform = `translateX(${offset}%)`;
     updateDots();
 }
 
 function updateDots() {
-    console.log("Updating dots for index:", currentIndex);
     dots.forEach((dot, index) => {
         dot.classList.toggle("active", index === currentIndex);
     });
@@ -32,13 +23,11 @@ function updateDots() {
 
 function nextSlide() {
     currentIndex = (currentIndex + 1) % totalBoxes;
-    console.log("Next slide, new index:", currentIndex);
     updateSliderPosition();
 }
 
 dots.forEach((dot, index) => {
     dot.addEventListener("click", () => {
-        console.log("Dot clicked, index:", index);
         currentIndex = index;
         updateSliderPosition();
         resetAutoSlide();
@@ -46,25 +35,21 @@ dots.forEach((dot, index) => {
 });
 
 function startAutoSlide() {
-    console.log("Starting auto-slide");
     autoSlide = setInterval(nextSlide, 5000);
 }
 
 function resetAutoSlide() {
-    console.log("Resetting auto-slide");
     clearInterval(autoSlide);
     startAutoSlide();
 }
 
-console.log("Initializing slider...");
+
 updateSliderPosition();
 startAutoSlide();
 
 slider.addEventListener('mouseenter', () => {
-    console.log("Hover: pausing auto-slide");
     clearInterval(autoSlide);
 });
 slider.addEventListener('mouseleave', () => {
-    console.log("Hover out: resuming auto-slide");
     startAutoSlide();
 });
