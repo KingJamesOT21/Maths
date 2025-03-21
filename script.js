3ed68b4222abbfe4b6c29fa8e3b7fe84a4e24fbf
KingJamesOT21 <KingJamesOT21@gmail.com>
2025-03-21 10:28:06 +0200

    responsiveness updates

--- script.js
+++ script.js
@@ -53,19 +53,25 @@ slider.addEventListener('mouseleave', () => {
     startAutoSlide();
 });
 
+
+
 const links = document.querySelector(".mobile-links");
 const menu = document.querySelector('.menu');
+const legal = document.querySelector('.mobile-legal')
 
 menu.addEventListener('click', function() {
     if (menu.classList.contains('menu')) {
         // Open the menu
         links.style.opacity = '1';
         links.style.pointerEvents = 'all';
+        legal.style.opacity = '1';
+        legal.style.pointerEvents = 'all';
         menu.classList.add('close');
         menu.classList.remove('menu');
         body.classList.add('no-scroll');
         html.classList.add('no-scroll');
-        window.scrollTo({ // Scroll to top
+        // Scroll to top
+        window.scrollTo({ 
             top: 0,
             behavior: 'smooth'
         });
@@ -73,6 +79,8 @@ menu.addEventListener('click', function() {
         // Close the menu
         links.style.opacity = '0';
         links.style.pointerEvents = 'none';
+        legal.style.opacity = '0';
+        legal.style.pointerEvents = 'none';
         menu.classList.remove('close');
         menu.classList.add('menu');
         body.classList.remove('no-scroll');
